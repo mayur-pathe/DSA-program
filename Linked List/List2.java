14a99a5ad979d6ed07a67f828d437c08405543a1
mayur-pathe <mayurpathe08@gmail.com>
2023-11-09 22:30:15 +0530

    Stack

---
+++ Linked List/List2.java
@@ -0,0 +1,74 @@
+import java.util.LinkedList;//Java Collection Framework
+public class List2 {
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    private Node getMid(Node head){
+        Node slow = head;
+        Node fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow; //mid node
+    }
+    private Node merge(Node head1 , Node head2){
+        Node mergedLL = new Node(-1);
+        Node temp = mergedLL;
+
+        while(head1 != null && head2 != null){
+            if(head1.data <= head2.data){
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            }else{
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+        while (head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+        while (head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return mergedLL.next;
+    }
+    public Node mergeSort(Node head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        //find mid
+        Node mid = getMid(head);
+        //left right MS
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+        //merge
+        return merge(newLeft,newRight);
+    }
+    public static void main(String[] args) {
+        //create 
+        LinkedList<Integer> ll = new LinkedList<>();
+        //add
+        ll.addLast(4);
+        ll.addLast(3);
+        ll.addLast(2);
+        ll.addLast(1);
+        ll.addFirst(5);
+        System.out.println(ll);
+
+    }
+}
